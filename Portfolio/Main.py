3fc16122d917240dd0317bddcae2c66b29c7852c
Zenn-976 <hadgal876@gmail.com>
2025-03-18 08:17:37 +0800

    Portfolio
    Hi

---
+++ Portfolio/Main.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(1,10)
+guess = input("1 - 10 hoorond taarah toog taa: ")
+guess = int(guess)
+if guess == number:
+    print('you won')
+else:
+    print(" woooow you are gay")
